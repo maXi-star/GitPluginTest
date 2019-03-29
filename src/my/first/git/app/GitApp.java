519211fa7f0a5fc04d61cae3301330970c367f8e
notrom <sk8er_fc.plus@yahoo.com>
2019-03-29 18:17:16 +0430

    first change in branch.

--- src/my/first/git/app/GitApp.java
+++ src/my/first/git/app/GitApp.java
@@ -3,6 +3,6 @@ package my.first.git.app;
 public class GitApp {
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("bye");
+		System.out.println("Hello Git, from branch bug#1234 !!");
 	}
 }
